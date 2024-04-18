019f815132b8565264d697c3364ca4913059315d
milko <gcalku@gmail.com>
2024-04-18 14:26:02 +0700

    first commit

---
+++ target/debug/deps/rust_basic-cf6eb7a878b69a65.d
@@ -0,0 +1,5 @@
+/home/milko/items/dev/rust/rust_basic/target/debug/deps/rust_basic-cf6eb7a878b69a65: src/main.rs
+
+/home/milko/items/dev/rust/rust_basic/target/debug/deps/rust_basic-cf6eb7a878b69a65.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust_basic.d
@@ -0,0 +1,1 @@
+/home/milko/items/dev/rust/rust_basic/target/debug/rust_basic: /home/milko/items/dev/rust/rust_basic/src/main.rs
